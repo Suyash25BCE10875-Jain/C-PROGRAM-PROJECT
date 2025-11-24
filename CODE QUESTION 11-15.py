b345dfbaceff0e122582e474d66a8aa95970a41d
Suyash25BCE10875-Jain <suyash.25bce10875@vitbhopal.ac.in>
2025-11-24 22:22:16 +0530

    Update CODE QUESTION 11-15.py

--- CODE QUESTION 11-15.py
+++ CODE QUESTION 11-15.py
@@ -1,5 +1,5 @@
 # Write a function count_distinct_prime_factors(n) that returns how many unique prime factors a number has.
-# SAMYAK JAIN (25BAI10746)
+# SUYASH JAIN (25BCE10875)
 import time
 import sys
 def prime_factors(n):
@@ -154,3 +154,4 @@ print(f"Number of divisors of {num}: {result}")
 print(f"Execution time: {end_time - start_time:.8f} seconds")
 print(f"Current memory usage: {current / 1024:.4f} KB")
 print(f"Peak memory usage: {peak /1024:.4f} KB")
+
